7a5e12ad2eacff8073fa6215d1445ac01544f55c
Alex And <alexandr_works@ukr.net>
2021-02-02 00:47:08 +0200

    init project

---
+++ src/components/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="container">
+		
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+	
+</style>
